2f5ee644dfaacb43949ae5900261e77bd5fcd7ee
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-15 10:13:22 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace App1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		border.StrokeDashArray.Add(5);
+		border.StrokeDashArray.Add(150);
+		border.StrokeDashArray.Add(300);
+		border.StrokeDashArray.Add(0);
+		border.StrokeDashOffset = 5;
+
+		
+		if (sender is Button button)
+		{
+			button.Text = "Clicked";
+		}
+	}
+}
